bbe52b4f70aeb428beeb72e5e473f82e6501ae70
Salman <salmankps2001@gmail.com>
2023-12-27 18:09:14 +0530

    Cors enabled

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-require('dotenv').config();
-//call and load env var
-// THIS IS WHERE THE ENTIRE BACKEND IS INTEGRATED INTO A SINGLE SERVER APPLICATION
-const connectToMongo = require('./db');
+const express = require('express');
+const app = express();
 const cors = require('cors');
-
 const mongoose = require('mongoose');
+const connectToMongo = require('./db');
+
+require('dotenv').config();
 
 connectToMongo();
 
-const express = require('express');
-const app = express();
+// Read allowed origins from environment variable
+const frontendHosts = process.env.FRONTEND_HOST || 'http://localhost:3000';
 
+// Split the comma-separated string into an array
+const whitelist = frontendHosts.split(',');
+console.log(whitelist)
 
-// TO USE req.body json to send data body with req 
-app.use(cors());
+// Configure CORS options
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 mongoose.set('strictQuery', true);
 
-// AVAILABLE ROUTES (APIs)
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notesroute'));
+
 app.get('/', (req, res) => {
-  res.send("We are connected to localhost")
+  res.send("We are connected to localhost");
 });
 
 const port = process.env.PORT || 3030;
 
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`Backend app started on ${port}`);
 });
-
